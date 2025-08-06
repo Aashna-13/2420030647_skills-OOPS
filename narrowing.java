d655f83731d11b285ed8a0125bf99944a34f270b
Aashna-13 <aashnabegum064@gmail.com>
2025-08-06 10:08:22 +0530

    Add files via upload

---
+++ narrowing.java
@@ -0,0 +1,13 @@
+package myproject;
+
+public class narrowing 
+{
+		    public static void main(String[] args) 
+		    {
+		        double myDouble = 53.832;
+		        int myInt = (int) myDouble;  // double to int
+		        System.out.println("Double: " + myDouble);
+		        System.out.println("Integer: " + myInt);
+		    }
+
+}
