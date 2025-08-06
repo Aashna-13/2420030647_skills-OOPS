92d424d9be8ece75f2e799d52f6cbbbb4154a03a
Aashna-13 <aashnabegum064@gmail.com>
2025-08-06 10:08:43 +0530

    Add files via upload

---
+++ widening.java
@@ -0,0 +1,13 @@
+package myproject;
+
+public class widening 
+{
+	    public static void main(String[] args) 
+	    {
+	        int myInt = 135;
+	        double myDouble = myInt;  // int to double
+	        System.out.println("Integer: " + myInt);
+	        System.out.println("Double: " + myDouble);
+	    }
+
+}
